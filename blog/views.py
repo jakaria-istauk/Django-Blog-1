702a3b4665977ba1ac76c72eff1cd69854c39fc2
unknown <jakariamd35@gmail.com>
2018-12-22 09:55:26 +0600

    Mogrations and Dinamic post

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Jakaria',
-        'title': 'Blog Post 1',
-        'content': 'Firsst post content',
-        'date_posted': 'Agust 28, 2018'
-    },
-    {
-        'author': 'Istauk',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'Agust 28, 2018'
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
